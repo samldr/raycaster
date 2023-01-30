e16a79332a2f31f0b4b01f2aa76780cc9f5f7d4d
samldr <sleader@ualberta.ca>
2023-01-30 16:16:55 -0700

    tried to add MUI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Menu } from './Menu'
+import { SceneMenu } from './Menu.tsx'
 import "./navbar.css"
 
 export const Navbar = () => {
   return (
     <div className='over'>
         <h1>Lightroom</h1>
-        <Menu/>
+        <SceneMenu/>
     </div>
   )
 }
